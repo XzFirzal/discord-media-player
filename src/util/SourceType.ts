c809e8c6dbd5a39ba28323f0b6beab6d34fcef87
XzFirzal <67226790+XzFirzal@users.noreply.github.com>
2021-08-08 20:13:38 +0700

    docs: new development documentation

--- src/util/SourceType.ts
+++
@@ -1,8 +0,0 @@
-/**
- * The source type of the audio source
- */
-export enum SourceType {
-  youtube = 0,
-  soundcloud = 1,
-  local = 2
-}
--- src/util/noop.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-
-/**
- * Empty function (NO-OP)
- */
-export function noop() {}
